3104349eef567f4c109be58013f441ebc1a4230b
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-03-14 12:46:01 -0600

    Agregando LogWriter
    Metodo para escribir el archivo "logs.txt"

--- EDLaboratorio3/Controllers/HomeController.cs
+++ EDLaboratorio3/Controllers/HomeController.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace EDLaboratorio3.Controllers
 {
     public class HomeController : Controller
     {
+        public static string ruta = @"C:\Users\" + Environment.UserName + @"\Desktop\logs.txt";
+
         public ActionResult Index()
         {
+            logWriter("VISITO INICIO", ruta, false);
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            logWriter("VISITO ACERCA DE", ruta, false);
 
             return View();
         }
 
         public ActionResult Contact()
         {
+            logWriter("VISITO CONTACTO", ruta, false);
+
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public static void logWriter(string contenido, string rutaArchivo, bool sobrescribir = true)
+        {
+            StreamWriter logReporter = new StreamWriter(rutaArchivo, !sobrescribir);
+            logReporter.WriteLine(contenido + "; " + DateTime.Now);
+            logReporter.Close();
         }
     }
 }
